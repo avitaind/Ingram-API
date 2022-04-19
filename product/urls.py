dc743d4acfd8ffb76e7220aec1006d9bd5aadffb
avitaind <52917489+avitaind@users.noreply.github.com>
2022-04-19 11:26:45 +0530

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -8,6 +8,8 @@ app_name = 'product'
 urlpatterns = [
     # ex: api/product/<str:token>/
     path('<str:token>/', views.ListProductView.as_view(), name='List Product'),
+    # ex: api/product/<str:token>/detail/
+    path('<str:token>/detail/', views.ProductDetailView.as_view(), name='Product Detail'),
     # ex: api/product/<str:token>/validate/
     path('<str:token>/validate/', views.ValidateSerialNoView.as_view(), name='Validate Product'),
     # ex: api/product/<str:token>/block/
